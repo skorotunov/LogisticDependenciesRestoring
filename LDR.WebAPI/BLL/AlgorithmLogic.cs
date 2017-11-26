88471e5ea8bb8a9537cc5f778d4ef629b6e8021c
Sergiy Korotunov <korotunovsergey@gmail.com>
2017-11-27 00:36:32 +0200

    Starting implementation of the basic logic

---
+++ LDR.WebAPI/BLL/AlgorithmLogic.cs
@@ -0,0 +1,25 @@
+﻿using LDR.WebAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LDR.WebAPI.BLL
+{
+    public class AlgorithmLogic
+    {
+        public void A1(Function function)
+        {
+            var fn = function.Points.OrderByDescending(x => x.Value.Value).First(); //check for null
+            
+            if (fn.Key == 1)
+            {
+                //A
+            }
+            else
+            {
+
+            }
+        }
+    }
+}
---
+++ LDR.WebAPI/Models/Function.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LDR.WebAPI.Models
+{
+    public class Function
+    {
+        public Function(int count)
+        {
+            this.PointsCount = count;
+            this.Points = new Dictionary<int, KeyValuePair<double, double>>(count);
+        }
+
+        public int PointsCount { get; set; }
+
+        public Dictionary<int, KeyValuePair<double, double>> Points { get; set; }
+    }
+}
