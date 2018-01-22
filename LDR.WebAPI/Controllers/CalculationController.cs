d8dfeed87142a19d64cce565ac83d3bd14ea80b0
Sergey Korotunov <korotunovsergey@gmail.com>
2018-01-22 12:30:29 +0200

    Add CRUD methods to the controller

--- LDR.WebAPI/Controllers/CalculationController.cs
+++ LDR.WebAPI/Controllers/CalculationController.cs
@@ -1,5 +1,6 @@
 ﻿using LDR.WebAPI.Models;
 using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -30,13 +31,80 @@ namespace LDR.WebAPI.Controllers
         [HttpGet("{id}", Name = "GetPoint")]
         public IActionResult GetById(long id)
         {
-            var item = this.context.Points.FirstOrDefault(x => x.Id == id);
+            Point point = this.context.Points.FirstOrDefault(x => x.Id == id);
+            if (point == null)
+            {
+                return this.NotFound();
+            }
+
+            return new ObjectResult(point);
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] Point point)
+        {
+            if (point == null)
+            {
+                return this.BadRequest();
+            }
+
+            this.context.Points.Add(point);
+            this.context.SaveChanges();
+
+            return this.CreatedAtRoute("GetPoint", new { id = point.Id }, point);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(long id, [FromBody] Point point)
+        {
+            if (point == null || point.Id != id)
+            {
+                return this.BadRequest();
+            }
+
+            Point item = null;
+            try
+            {
+                item = this.context.Points.SingleOrDefault(x => x.Id == id);
+            }
+            catch (InvalidOperationException)
+            {
+                return this.StatusCode(500);
+            }
+
             if (item == null)
             {
                 return this.NotFound();
             }
 
-            return new ObjectResult(item);
+            item.X = point.X;
+            item.Y = point.Y;
+            this.context.Points.Update(item);
+            this.context.SaveChanges();
+            return new NoContentResult();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(long id)
+        {
+            Point point = null;
+            try
+            {
+                point = this.context.Points.SingleOrDefault(x => x.Id == id);
+            }
+            catch (InvalidOperationException)
+            {
+                return this.StatusCode(500);
+            }
+
+            if (point == null)
+            {
+                return this.NotFound();
+            }
+
+            this.context.Points.Remove(point);
+            this.context.SaveChanges();
+            return new NoContentResult();
         }
     }
 }
