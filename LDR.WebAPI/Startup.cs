000ecbf7d2ecf0a49ffed884f7d8d495bf96361f
Sergey Korotunov <korotunovsergey@gmail.com>
2018-01-22 12:37:27 +0200

    Swagger introduced

--- LDR.WebAPI/Startup.cs
+++ LDR.WebAPI/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace LDR.WebAPI
 {
@@ -12,10 +13,19 @@ namespace LDR.WebAPI
         {
             services.AddDbContext<LDRContext>(x => x.UseInMemoryDatabase("LogisticDependenciesRestoring"));
             services.AddMvc();
+            services.AddSwaggerGen(x =>
+            {
+                x.SwaggerDoc("v1", new Info { Title = "LDR API", Version = "v1" });
+            });
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseSwagger();
+            app.UseSwaggerUI(x =>
+            {
+                x.SwaggerEndpoint("/swagger/v1/swagger.json", "LDR API V1");
+            });
             app.UseMvc();
         }
     }
